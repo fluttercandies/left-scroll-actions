4e976727fe63da3598f15945c2f60b8f05beda71
MaJialun <mjl0602@gmail.com>
2021-07-06 18:42:10 +0800

    [2.0.0]🔧Fix: 发布正式版本

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/majialun/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/majialun/Documents/code/opensource/left-scroll-actions/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
